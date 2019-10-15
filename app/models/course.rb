c356d16ef1806ddba31dd64c6006db425f1168a0
Seybou <seybou@yahoo.com>
2019-10-15 05:26:00 +0000

    Lesson 17

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,8 @@
 class Course < ApplicationRecord
     belongs_to :user
-    validates :title, presence: true
-    validates :description, presence: true
-    validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
+    has_many   :sections
+    validates  :title, presence: true
+    validates  :description, presence: true
+    validates  :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
 end
 
